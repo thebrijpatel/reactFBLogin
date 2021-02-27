ae470256607ecb4aaf0b81b8b51ac6e9d2ff41d9
Brijesh Patel <sadariyabrijesh@gmail.com>
2021-02-27 10:49:20 +0530

    finishing up

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Facebook from './components/Facebook';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Facebook Auth example</h1>
+        <p>To get started, autheticate with facebook</p>
+        <Facebook />
+      </header>
+    </div>
+  );
+}
+
+export default App;
